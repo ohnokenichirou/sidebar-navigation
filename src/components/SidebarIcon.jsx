4d1213418330ce0d463b9001d20722e1d7c0827f
ohnokenichirou <99148127+ohno-kenichirou@users.noreply.github.com>
2024-09-13 18:02:40 +0900

    Reactでサイドバーナビゲーションの作り方！【React入門】-プログラミングチュートリアル

---
+++ src/components/SidebarIcon.jsx
@@ -0,0 +1,10 @@
+import Icon from "../images/icon.png";
+
+export const SidebarIcon = () => {
+  return (
+    <div className="SidebarIcon">
+      <img src={Icon} />
+      <p>programming@gmail.com</p>
+    </div>
+  );
+};
